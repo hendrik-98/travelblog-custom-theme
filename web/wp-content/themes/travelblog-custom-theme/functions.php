fa8c4189e47ebcdb187a94c9542594283626c702
hendrik-98 <hendrikegbers18@googlemail.com>
2024-03-06 08:35:46 +0100

    add metafields in functions.php

--- web/wp-content/themes/travelblog-custom-theme/functions.php
+++ web/wp-content/themes/travelblog-custom-theme/functions.php
@@ -6,3 +6,7 @@ include_once( get_template_directory() . '/functions/settings.php' );
 //Custom Post Type einbinden
 include_once( get_template_directory() . '/posttypes/hotels/hotels.php' );
 include_once( get_template_directory() . '/posttypes/events/events.php' );
+
+//Custom  Metafield einbinden
+include_once( get_template_directory() . '/posttypes/hotels/metafields/hotels-metafield.php' );
+include_once( get_template_directory() . '/posttypes/events/metafields/events-metafield.php' );
